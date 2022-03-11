1210c8e56ae57ac4221dffa5c5fcd098b3fb6242
SkyeBeFreeman <928016560@qq.com>
2022-03-11 18:12:26 +0800

    fix:fix services of ServicesByProto cannot be modified error.

--- polaris-common/polaris-model/src/main/java/com/tencent/polaris/client/pojo/ServicesByProto.java
+++ polaris-common/polaris-model/src/main/java/com/tencent/polaris/client/pojo/ServicesByProto.java
@@ -32,19 +32,22 @@ import java.util.Objects;
 
 public class ServicesByProto implements Services, RegistryCacheValue {
 
-    private ServiceKey svcKey;
-
     private final List<ServiceInfo> services;
-
     private final boolean initialized;
-
     private final boolean loadedFromFile;
-
     private final int hashCode;
+    private ServiceKey svcKey;
 
     public ServicesByProto() {
         this.services = Collections.emptyList();
         this.initialized = false;
+        this.loadedFromFile = false;
+        this.hashCode = 0;
+    }
+
+    public ServicesByProto(List<ServiceInfo> services) {
+        this.services = services;
+        this.initialized = true;
         this.loadedFromFile = false;
         this.hashCode = 0;
     }
