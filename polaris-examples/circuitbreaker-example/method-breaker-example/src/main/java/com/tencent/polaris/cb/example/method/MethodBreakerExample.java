edefa424e6a27544819c4af309dbc7824355deeb
seanyu <seansyyu@tencent.com>
2023-02-28 11:16:33 +0800

    revoke exmaple

--- polaris-examples/circuitbreaker-example/method-breaker-example/src/main/java/com/tencent/polaris/cb/example/method/MethodBreakerExample.java
+++ polaris-examples/circuitbreaker-example/method-breaker-example/src/main/java/com/tencent/polaris/cb/example/method/MethodBreakerExample.java
@@ -31,8 +31,8 @@ public class MethodBreakerExample {
 
         CircuitBreakAPI circuitBreakAPI = CircuitBreakAPIFactory.createCircuitBreakAPI();
         FunctionalDecoratorRequest makeDecoratorRequest = new FunctionalDecoratorRequest();
-        makeDecoratorRequest.setService(new ServiceKey("default", "EchoServerJava"));
-        makeDecoratorRequest.setMethod("echo");
+        makeDecoratorRequest.setService(new ServiceKey("default", "testSvc2"));
+        makeDecoratorRequest.setMethod("foo");
         FunctionalDecorator decorator = circuitBreakAPI.makeFunctionalDecorator(makeDecoratorRequest);
         Consumer<Boolean> integerConsumer = decorator.decorateConsumer(new Consumer<Boolean>() {
             @Override
