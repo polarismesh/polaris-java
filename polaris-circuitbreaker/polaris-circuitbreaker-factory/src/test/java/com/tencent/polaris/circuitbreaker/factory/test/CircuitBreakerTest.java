b14aa8697c3ac773186f2bb2315539b076bdf4b4
seanyu <seansyyu@tencent.com>
2023-03-08 17:57:13 +0800

    add ut

--- polaris-circuitbreaker/polaris-circuitbreaker-factory/src/test/java/com/tencent/polaris/circuitbreaker/factory/test/CircuitBreakerTest.java
+++ polaris-circuitbreaker/polaris-circuitbreaker-factory/src/test/java/com/tencent/polaris/circuitbreaker/factory/test/CircuitBreakerTest.java
@@ -21,6 +21,7 @@ import static com.tencent.polaris.test.common.Consts.NAMESPACE_TEST;
 import static com.tencent.polaris.test.common.Consts.SERVICE_CIRCUIT_BREAKER;
 import static com.tencent.polaris.test.common.TestUtils.SERVER_ADDRESS_ENV;
 
+import com.google.protobuf.util.JsonFormat;
 import com.tencent.polaris.api.config.Configuration;
 import com.tencent.polaris.api.config.plugin.DefaultPlugins;
 import com.tencent.polaris.api.core.ConsumerAPI;
@@ -31,15 +32,29 @@ import com.tencent.polaris.api.pojo.ServiceKey;
 import com.tencent.polaris.api.rpc.GetInstancesRequest;
 import com.tencent.polaris.api.rpc.InstancesResponse;
 import com.tencent.polaris.api.rpc.ServiceCallResult;
+import com.tencent.polaris.circuitbreak.api.CircuitBreakAPI;
+import com.tencent.polaris.circuitbreak.api.FunctionalDecorator;
+import com.tencent.polaris.circuitbreak.api.pojo.FunctionalDecoratorRequest;
+import com.tencent.polaris.circuitbreak.client.exception.CallAbortedException;
+import com.tencent.polaris.circuitbreak.factory.CircuitBreakAPIFactory;
 import com.tencent.polaris.client.util.Utils;
 import com.tencent.polaris.factory.api.DiscoveryAPIFactory;
 import com.tencent.polaris.factory.config.ConfigurationImpl;
 import com.tencent.polaris.logging.LoggerFactory;
+import com.tencent.polaris.specification.api.v1.fault.tolerance.CircuitBreakerProto;
 import com.tencent.polaris.test.common.TestUtils;
 import com.tencent.polaris.test.mock.discovery.NamingServer;
 import com.tencent.polaris.test.mock.discovery.NamingService.InstanceParameter;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -61,7 +76,7 @@ public class CircuitBreakerTest {
     private NamingServer namingServer;
 
     @Before
-    public void before() {
+    public void before() throws IOException {
         try {
             namingServer = NamingServer.startNamingServer(-1);
             System.setProperty(SERVER_ADDRESS_ENV, String.format("127.0.0.1:%d", namingServer.getPort()));
@@ -74,6 +89,15 @@ public class CircuitBreakerTest {
         parameter.setIsolated(false);
         parameter.setWeight(100);
         namingServer.getNamingService().batchAddInstances(serviceKey, 10010, MAX_COUNT, parameter);
+
+        CircuitBreakerProto.CircuitBreakerRule.Builder circuitBreakerRuleBuilder =  CircuitBreakerProto.CircuitBreakerRule.newBuilder();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("circuitBreakerRule.json");
+        String json = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8)).lines().collect(Collectors.joining(""));
+        JsonFormat.parser().ignoringUnknownFields().merge(json, circuitBreakerRuleBuilder);
+        CircuitBreakerProto.CircuitBreakerRule circuitBreakerRule = circuitBreakerRuleBuilder.build();
+        CircuitBreakerProto.CircuitBreaker circuitBreaker = CircuitBreakerProto.CircuitBreaker.newBuilder().addRules(circuitBreakerRule).build();
+        namingServer.getNamingService().setCircuitBreaker(serviceKey, circuitBreaker);
+
     }
 
     @After
@@ -232,6 +256,32 @@ public class CircuitBreakerTest {
             instances = consumerAPI.getInstances(getInstancesRequest);
             Assert.assertEquals(MAX_COUNT, instances.getInstances().length);
         }
+    }
+
+    @Test
+    public void testFunctionalDecorator() {
+        Configuration configuration = TestUtils.configWithEnvAddress();
+        CircuitBreakAPI circuitBreakAPI = CircuitBreakAPIFactory.createCircuitBreakAPIByConfig(configuration);
+        FunctionalDecoratorRequest makeDecoratorRequest = new FunctionalDecoratorRequest(new ServiceKey(NAMESPACE_TEST, SERVICE_CIRCUIT_BREAKER), "'");
+        FunctionalDecorator decorator = circuitBreakAPI.makeFunctionalDecorator(makeDecoratorRequest);
+        Consumer<Integer> integerConsumer = decorator.decorateConsumer(num -> {
+            if (num % 2 == 0) {
+                throw new IllegalArgumentException("invoke failed");
+            } else {
+                System.out.println("invoke success");
+            }
+        });
+        try {
+            integerConsumer.accept(1);
+            Utils.sleepUninterrupted(1000);
+            integerConsumer.accept(2);
+            Utils.sleepUninterrupted(1000);
+        }catch (Exception e){
+            if (!(e instanceof IllegalArgumentException)){
+                throw e;
+            }
+        }
+        Assert.assertThrows(CallAbortedException.class, () -> integerConsumer.accept(3));
     }
 
 }
