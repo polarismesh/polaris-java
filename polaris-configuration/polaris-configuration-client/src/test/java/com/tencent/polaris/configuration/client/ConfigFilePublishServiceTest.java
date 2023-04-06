f2cba4efb2092261c4285b2bbd1b746af0257e71
fabian4 <fabian4@163.com>
2023-04-06 16:51:41 +0800

    add test for upsertConfigFile

---
+++ polaris-configuration/polaris-configuration-client/src/test/java/com/tencent/polaris/configuration/client/ConfigFilePublishServiceTest.java
@@ -0,0 +1,157 @@
+/*
+ * Tencent is pleased to support the open source community by making Polaris available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.polaris.configuration.client;
+
+import com.tencent.polaris.configuration.api.core.ConfigFile;
+import com.tencent.polaris.configuration.client.internal.ConfigFileManager;
+import com.tencent.polaris.configuration.client.internal.DefaultConfigFileMetadata;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+/**
+ * @author fabian4 2023-04-06
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ConfigFilePublishServiceTest {
+
+    @InjectMocks
+    private DefaultConfigFilePublishService defaultConfigFilePublishService;
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateNamespaceBlank() {
+        defaultConfigFilePublishService.createConfigFile("", "somegroup", "application.yaml", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateGroupBlank() {
+        defaultConfigFilePublishService.createConfigFile("somenamespace", "", "application.yaml", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateFileNameBlank() {
+        defaultConfigFilePublishService.createConfigFile("somenamespace", "somegroup", "", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateNamespaceBlank2() {
+        defaultConfigFilePublishService.createConfigFile(new DefaultConfigFileMetadata("", "somegroup", "application.yaml"), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateGroupBlank2() {
+        defaultConfigFilePublishService.createConfigFile(new DefaultConfigFileMetadata("somenamespace", "", "application.yaml"), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCreateFileNameBlank2() {
+        defaultConfigFilePublishService.createConfigFile(new DefaultConfigFileMetadata("somenamespace", "somegroup", ""), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdateNamespaceBlank() {
+        defaultConfigFilePublishService.updateConfigFile("", "somegroup", "application.yaml", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdateGroupBlank() {
+        defaultConfigFilePublishService.updateConfigFile("somenamespace", "", "application.yaml", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdateFileNameBlank() {
+        defaultConfigFilePublishService.updateConfigFile("somenamespace", "somegroup", "", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdateNamespaceBlank2() {
+        defaultConfigFilePublishService.updateConfigFile(new DefaultConfigFileMetadata("", "somegroup", "application.yaml"), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdateGroupBlank2() {
+        defaultConfigFilePublishService.updateConfigFile(new DefaultConfigFileMetadata("somenamespace", "", "application.yaml"), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdateFileNameBlank2() {
+        defaultConfigFilePublishService.updateConfigFile(new DefaultConfigFileMetadata("somenamespace", "somegroup", ""), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpsertNamespaceBlank() {
+        defaultConfigFilePublishService.upsertConfigFile("", "somegroup", "application.yaml", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpsertGroupBlank() {
+        defaultConfigFilePublishService.upsertConfigFile("somenamespace", "", "application.yaml", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpsertFileNameBlank() {
+        defaultConfigFilePublishService.upsertConfigFile("somenamespace", "somegroup", "", "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpsertNamespaceBlank2() {
+        defaultConfigFilePublishService.upsertConfigFile(new DefaultConfigFileMetadata("", "somegroup", "application.yaml"), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpsertGroupBlank2() {
+        defaultConfigFilePublishService.upsertConfigFile(new DefaultConfigFileMetadata("somenamespace", "", "application.yaml"), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpsertFileNameBlank2() {
+        defaultConfigFilePublishService.upsertConfigFile(new DefaultConfigFileMetadata("somenamespace", "somegroup", ""), "content");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReleaseNamespaceBlank() {
+        defaultConfigFilePublishService.releaseConfigFile("", "somegroup", "application.yaml");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReleaseGroupBlank() {
+        defaultConfigFilePublishService.releaseConfigFile("somenamespace", "", "application.yaml");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReleaseFileNameBlank() {
+        defaultConfigFilePublishService.releaseConfigFile("somenamespace", "somegroup", "");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReleaseNamespaceBlank2() {
+        defaultConfigFilePublishService.releaseConfigFile(new DefaultConfigFileMetadata("", "somegroup", "application.yaml"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReleaseGroupBlank2() {
+        defaultConfigFilePublishService.releaseConfigFile(new DefaultConfigFileMetadata("somenamespace", "", "application.yaml"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testReleaseFileNameBlank2() {
+        defaultConfigFilePublishService.releaseConfigFile(new DefaultConfigFileMetadata("somenamespace", "somegroup", ""));
+    }
+}
