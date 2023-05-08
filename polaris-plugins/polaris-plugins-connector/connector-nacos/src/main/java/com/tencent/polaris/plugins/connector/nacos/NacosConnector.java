d93ec0605d86c7e91a5b87d83a82b45feae9d81e
vitaswan <vitaswan@tencent.com>
2023-05-08 19:13:43 +0800

    fix:多注册中心nacos与polaris支持不同namespace

--- polaris-plugins/polaris-plugins-connector/connector-nacos/src/main/java/com/tencent/polaris/plugins/connector/nacos/NacosConnector.java
+++ polaris-plugins/polaris-plugins-connector/connector-nacos/src/main/java/com/tencent/polaris/plugins/connector/nacos/NacosConnector.java
@@ -239,7 +239,8 @@ public class NacosConnector extends DestroyableServerConnector {
 	}
 
 	@Override
-	public CommonProviderResponse registerInstance(CommonProviderRequest req, Map<String, String> customHeader) throws PolarisException {
+	public CommonProviderResponse registerInstance(CommonProviderRequest req,
+					Map<String, String> customHeader) throws PolarisException {
 		CommonProviderResponse response = new CommonProviderResponse();
 
 		if (isRegisterEnable()) {
