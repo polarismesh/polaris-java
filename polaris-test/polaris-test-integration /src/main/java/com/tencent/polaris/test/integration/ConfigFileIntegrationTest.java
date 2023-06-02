d91d571bd9db4007c3d198e9917b3cc2916ac778
fabian4 <fabian4@163.com>
2023-06-02 16:17:12 +0800

    add integration test

---
+++ polaris-test/polaris-test-integration /src/main/java/com/tencent/polaris/test/integration/ConfigFileIntegrationTest.java
@@ -0,0 +1,15 @@
+package com.tencent.polaris.configuration.example;
+
+/**
+ * 运行前请修改 polaris.yml 中的北极星服务地址
+ *
+ * @author fabian 2023-06-02
+ */
+public class ConfigFileIntegrationTest {
+
+
+	public static void main(String[] args) throws Exception {
+
+	}
+
+}
