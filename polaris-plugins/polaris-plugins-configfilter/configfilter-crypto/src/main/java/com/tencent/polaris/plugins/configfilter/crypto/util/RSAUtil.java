7477df27d50b7388705a7c2984b05eafd8c97025
fabian4 <fabian4@163.com>
2023-06-14 23:59:55 +0800

    feat: add support for configFile encryption.

--- polaris-plugins/polaris-plugins-configfilter/configfilter-crypto/src/main/java/com/tencent/polaris/plugins/configfilter/crypto/util/RSAUtil.java
+++ polaris-plugins/polaris-plugins-configfilter/configfilter-crypto/src/main/java/com/tencent/polaris/plugins/configfilter/crypto/util/RSAUtil.java
@@ -17,18 +17,21 @@
 
 package com.tencent.polaris.plugins.configfilter.crypto.util;
 
-import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import com.tencent.polaris.api.exception.ErrorCode;
+import com.tencent.polaris.api.exception.PolarisException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.SecureRandom;
 import java.security.Security;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 
 /**
@@ -37,62 +40,62 @@ import java.util.Base64;
  */
 public class RSAUtil {
 
-//RSAUtil    /**
-//     * 生成RSA密钥对
-//     */
-//    public static KeyPair generateRsaKeyPair() {
-//        KeyPairGenerator keyPairGenerator;
-//        try {
-//            keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-//        } catch (NoSuchAlgorithmException e) {
-//            throw new PolarisException(ErrorCode.RSA_KEY_GENERATE_ERROR, e.getMessage());
-//        }
-//        keyPairGenerator.initialize(1024);
-//        return keyPairGenerator.generateKeyPair();
-//    }
-//
-//    /**
-//     * RSA加密
-//     *
-//     * @param content   需要加密的内容
-//     * @param publicKey 公钥
-//     */
-//    public static byte[] encrypt(byte[] content, PublicKey publicKey) {
-//        try {
-//            Cipher cipher = Cipher.getInstance("RSA");
-//            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-//            return cipher.doFinal(content);
-//        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
-//                 BadPaddingException e) {
-//            throw new PolarisException(ErrorCode.RSA_ENCRYPT_ERROR, e.getMessage());
-//        }
-//    }
-//
-//    /**
-//     * RSA解密
-//     *
-//     * @param content    待解密内容
-//     * @param privateKey 私钥
-//     */
-//    public static byte[] decrypt(byte[] content, PrivateKey privateKey) {
-//        try {
-//            Cipher cipher = Cipher.getInstance("RSA");
-//            cipher.init(Cipher.DECRYPT_MODE, privateKey);
-//            return cipher.doFinal(content);
-//        } catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | IllegalBlockSizeException |
-//                 BadPaddingException e) {
-//            throw new PolarisException(ErrorCode.RSA_DECRYPT_ERROR, e.getMessage());
-//        }
-//    }
+    /**
+     * 生成RSA密钥对
+     */
+    public static KeyPair generateRsaKeyPair() {
+        KeyPairGenerator keyPairGenerator;
+        try {
+            keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException e) {
+            throw new PolarisException(ErrorCode.RSA_KEY_GENERATE_ERROR, e.getMessage());
+        }
+        keyPairGenerator.initialize(1024);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    /**
+     * RSA加密
+     *
+     * @param content   需要加密的内容
+     * @param publicKey 公钥
+     */
+    public static byte[] encrypt(byte[] content, PublicKey publicKey) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            return cipher.doFinal(content);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new PolarisException(ErrorCode.RSA_ENCRYPT_ERROR, e.getMessage());
+        }
+    }
+
+    /**
+     * RSA解密
+     *
+     * @param content    待解密内容
+     * @param privateKey 私钥
+     */
+    public static byte[] decrypt(byte[] content, PrivateKey privateKey) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            return cipher.doFinal(content);
+        } catch (NoSuchAlgorithmException | InvalidKeyException | NoSuchPaddingException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new PolarisException(ErrorCode.RSA_DECRYPT_ERROR, e.getMessage());
+        }
+    }
 
     public static void main(String[] args) throws Exception {
-        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA", BouncyCastleProvider.PROVIDER_NAME);
-        keyPairGen.initialize(1024, new SecureRandom());
-        KeyPair keyPair = keyPairGen.generateKeyPair();
+        Security.addProvider(new BouncyCastleProvider());
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(1024);
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        System.out.println(Base64.getEncoder().encodeToString(publicKey.getEncoded()));
 
-        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate(); // 得到私钥
-
-        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic(); // 得到公钥
 
     }
 }
