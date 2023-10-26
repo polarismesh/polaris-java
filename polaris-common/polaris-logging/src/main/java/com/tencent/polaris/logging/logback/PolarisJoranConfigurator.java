7b64020dbf0bba13f0cd7eb86a39a3f52b48b69d
Haotian Zhang <928016560@qq.com>
2023-10-26 13:25:58 +0800

    feat:optimize logback. (#438)

---
+++ polaris-common/polaris-logging/src/main/java/com/tencent/polaris/logging/logback/PolarisJoranConfigurator.java
@@ -0,0 +1,62 @@
+/*
+ * Tencent is pleased to support the open source community by making Polaris available.
+ *
+ * Copyright (C) 2019 THL A29 Limited, a Tencent company. All rights reserved.
+ *
+ * Licensed under the BSD 3-Clause License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.tencent.polaris.logging.logback;
+
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.event.SaxEvent;
+import ch.qos.logback.core.joran.spi.JoranException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+
+/**
+ * @author Haotian Zhang
+ */
+public class PolarisJoranConfigurator extends JoranConfigurator {
+
+    @Override
+    public void registerSafeConfiguration(List<SaxEvent> eventList) {
+    }
+
+    public void doPolarisConfigure(URL url) throws JoranException {
+        InputStream in = null;
+        try {
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setUseCaches(false);
+            in = urlConnection.getInputStream();
+            doConfigure(in, url.toExternalForm());
+        } catch (IOException ioe) {
+            String errMsg = "Could not open URL [" + url + "].";
+            addError(errMsg, ioe);
+            throw new JoranException(errMsg, ioe);
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ioe) {
+                    String errMsg = "Could not close [" + url + "] stream";
+                    addError(errMsg, ioe);
+                    throw new JoranException(errMsg, ioe);
+                }
+            }
+        }
+    }
+}
