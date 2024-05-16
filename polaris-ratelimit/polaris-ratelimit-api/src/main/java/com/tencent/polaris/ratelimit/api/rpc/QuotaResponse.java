8bb10a61d82ad9eaa9f5d847c8ee3e6f3ca2eb39
fredrikliu <fredrikliu@tencent.com>
2024-05-16 14:24:34 +0800

    change style

--- polaris-ratelimit/polaris-ratelimit-api/src/main/java/com/tencent/polaris/ratelimit/api/rpc/QuotaResponse.java
+++ polaris-ratelimit/polaris-ratelimit-api/src/main/java/com/tencent/polaris/ratelimit/api/rpc/QuotaResponse.java
@@ -47,7 +47,6 @@ public class QuotaResponse {
         this.activeRule = activeRule;
     }
 
-
     public String getActiveRuleName() {
         String activeRuleName = this.getActiveRule().getName().getValue();
 
