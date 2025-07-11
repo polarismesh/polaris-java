cec25137759de20b0d4378d5a5b32251924c4d85
fishtailfu <fishtailfu@tencent.com>
2025-07-11 16:49:48 +0800

    fix: fix prometheus reporter test1

--- polaris-plugins/polaris-plugins-observability/stat-prometheus/src/test/java/com/tencent/polaris/plugins/stat/prometheus/plugin/PrometheusReporterTest.java
+++ polaris-plugins/polaris-plugins-observability/stat-prometheus/src/test/java/com/tencent/polaris/plugins/stat/prometheus/plugin/PrometheusReporterTest.java
@@ -105,7 +105,7 @@ public class PrometheusReporterTest {
         ServiceCallResult callResult = mockServiceCallResult();
         statInfo.setRouterGauge(callResult);
         batchDone(() -> handler.reportStat(statInfo), count);
-
+        // for github action
         // mock push
         LOG.info("first mock push finish...");
         Thread.sleep(pushInterval + 50);
