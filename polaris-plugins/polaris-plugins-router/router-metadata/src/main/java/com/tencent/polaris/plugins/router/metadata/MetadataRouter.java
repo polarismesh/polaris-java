6d0e9a6eb396fbdc7d3eab6cdc30ff5e1d915d7f
fishtailfu <fishtailfu@tencent.com>
2025-09-15 15:04:17 +0800

    fix: stop using dest-svc-metadata to match instance metadata.

--- polaris-plugins/polaris-plugins-router/router-metadata/src/main/java/com/tencent/polaris/plugins/router/metadata/MetadataRouter.java
+++ polaris-plugins/polaris-plugins-router/router-metadata/src/main/java/com/tencent/polaris/plugins/router/metadata/MetadataRouter.java
@@ -183,12 +183,7 @@ public class MetadataRouter extends AbstractServiceRouter implements PluginConfi
     }
 
     private Map<String, String> getRouterMetadata(RouteInfo routeInfo) {
-        //兼容从 destService 获取元数据
-        Map<String, String> metadata = routeInfo.getDestService().getMetadata();
-        if (MapUtils.isNotEmpty(metadata)) {
-            return metadata;
-        }
-        metadata = new HashMap<>(routeInfo.getRouterMetadata(ROUTER_TYPE_METADATA));
+        Map<String, String> metadata = new HashMap<>(routeInfo.getRouterMetadata(ROUTER_TYPE_METADATA));
         if (routeInfo.getMetadataContainerGroup() != null && routeInfo.getMetadataContainerGroup().getCustomMetadataContainer() != null) {
             String metadataRouteKeys = routeInfo.getMetadataContainerGroup().getCustomMetadataContainer().getRawMetadataMapValue(ROUTER_TYPE_METADATA, KEY_METADATA_KEYS);
             if (StringUtils.isNotBlank(metadataRouteKeys)) {
